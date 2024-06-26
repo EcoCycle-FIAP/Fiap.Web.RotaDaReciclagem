18e3b8dfd69deb5ea5917d60584544dc74bb2187
adrian-siv <adrian.ssiv@gmail.com>
2024-06-26 08:48:46 -0300

    feat: Setup Database environment

---
+++ Data/Contexts/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Web.RotaDaReciclagem.Data.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected DatabaseContext()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
+using Fiap.Web.RotaDaReciclagem.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
