fa339d3c1ecf517125155796dc6f9ad8b6e61a1b
adrian-siv <adrian.ssiv@gmail.com>
2024-06-26 13:58:06 -0300

    feat: add Caminhoes table on DB

---
+++ Migrations/20240626165604_AddCaminhoes.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.Web.RotaDaReciclagem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCaminhoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Caminhoes",
+                columns: table => new
+                {
+                    CaminhaoId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Motorista = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Modelo = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Placa = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CapacidadeLitros = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caminhoes", x => x.CaminhaoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Caminhoes");
+        }
+    }
+}
