5ba05b5c45e5582370c8b499b6e33638c5001112
adrian-siv <adrian.ssiv@gmail.com>
2024-06-26 13:57:57 -0300

    feat: add Moradores table on DB

--- Data/Contexts/DatabaseContext.cs
+++ Data/Contexts/DatabaseContext.cs
@@ -1,9 +1,39 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fiap.Web.RotaDaReciclagem.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fiap.Web.RotaDaReciclagem.Data.Contexts
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<MoradorModel> Moradores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoradorModel>(entity =>
+            {
+                entity.ToTable("Moradores");
+                entity.HasKey(e => e.MoradorId);
+                entity.Property(e => e.Nome).IsRequired();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.Telefone).IsRequired().HasMaxLength(15);
+                entity.Property(e => e.Senha).IsRequired();
+                entity.Property(e => e.EnderecoBairro).IsRequired();
+                entity.Property(e => e.Endereco).IsRequired();
+                entity.Property(e => e.EnderecoNumero).IsRequired().HasColumnType("INTEGER");
+                entity.Property(e => e.EnderecoComplemento).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<CaminhaoModel>(entity =>
+            {
+                entity.ToTable("Caminhoes");
+                entity.HasKey(e => e.CaminhaoId);
+                entity.Property(e => e.Motorista).IsRequired();
+                entity.Property(e => e.Modelo).IsRequired();
+                entity.Property(e => e.Placa).IsRequired();
+                entity.Property(e => e.CapacidadeLitros).IsRequired();
+            });
+        }
+
         public DatabaseContext(DbContextOptions options) : base(options)
         {
         }
---
+++ Migrations/20240626164832_AddMoradores.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.Web.RotaDaReciclagem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoradores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moradores",
+                columns: table => new
+                {
+                    MoradorId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Endereco = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    EnderecoNumero = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnderecoComplemento = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    EnderecoBairro = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Telefone = table.Column<string>(type: "NVARCHAR2(15)", maxLength: 15, nullable: false),
+                    Senha = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moradores", x => x.MoradorId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moradores");
+        }
+    }
+}
