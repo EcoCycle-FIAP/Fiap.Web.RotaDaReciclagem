d7487d90fec48971c7e609904cb4f758a283ff43
adrian-siv <adrian.ssiv@gmail.com>
2024-06-26 07:45:33 -0300

    feat: create basic Controllers

---
+++ Controllers/CaminhaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.RotaDaReciclagem.Controllers
+{
+    public class CaminhaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
