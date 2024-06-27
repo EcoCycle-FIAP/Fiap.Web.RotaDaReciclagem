564c149f4d27474724e4c5c628a71e1f192368b2
Nicole Ribeiro Tuccio <98128263+NicoleTuccio@users.noreply.github.com>
2024-06-26 21:04:43 -0300

    morador get

--- Controllers/MoradorController.cs
+++ Controllers/MoradorController.cs
@@ -4,8 +4,13 @@ namespace Fiap.Web.RotaDaReciclagem.Controllers
 {
     public class MoradorController : Controller
     {
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
+            Console.WriteLine(_moradores.Count);
+            return View(_moradores);
+        }
+
+        [HttpGet]
+        public ActionResult Create() {
             return View();
         }
     }
